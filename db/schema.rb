7cd3f664c02d05f974659f4ab69c3b3874e949ff
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-08-14 19:50:59 -0600

    changes cc number to string

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_232927) do
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "invoice_id"
-    t.bigint "credit_card_number"
+    t.string "credit_card_number"
     t.integer "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
