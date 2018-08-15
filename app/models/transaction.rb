896935edc0ae80bafed6873198b0fba543f4044b
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-08-15 14:31:24 -0600

    adds has many relationships to the customer, invoice, transaction models.  adds scope to transaction model.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,8 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   enum result: [:failed, :success]
+
+  default_scope -> { order(id: :asc) }
+  
+  scope :success, -> { where(result: 1) }
 end
