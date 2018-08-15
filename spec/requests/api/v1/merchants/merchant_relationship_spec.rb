d2bddb9c583e866732394e654f92e37d3913f667
brickstar <brickstar@gmail.com>
2018-08-15 15:40:04 -0600

    merchant invoices spec

--- spec/requests/api/v1/merchants/merchant_relationship_spec.rb
+++ spec/requests/api/v1/merchants/merchant_relationship_spec.rb
@@ -17,4 +17,21 @@ describe "Merchants relationships API" do
     expect(items.first).to_not have_key(:created_at)
     expect(items.first).to_not have_key(:updated_at)
   end
+
+  it "sends a list of invoices for a merchant" do
+    merchant = create(:merchant)
+    invoices = create_list(:invoice, 3, merchant: merchant)
+
+    get "/api/v1/merchants/#{merchant.id}/invoices"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body, symbolize_names: true)
+
+    expect(invoices.count).to eq(3)
+    expect(invoices.first).to have_key(:id)
+    expect(invoices.first).to have_key(:name)
+    expect(invoices.first).to_not have_key(:created_at)
+    expect(invoices.first).to_not have_key(:updated_at)
+  end
 end
