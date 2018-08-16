41209c711e684821d60ffa041bfd8651a88b08bc
brickstar <brickstar@gmail.com>
2018-08-16 09:48:20 -0600

    add edge case build for total revenue serializer and spec

--- app/serializers/merchant_date_serializer.rb
+++ app/serializers/merchant_date_serializer.rb
@@ -2,6 +2,7 @@ class MerchantDateSerializer < ActiveModel::Serializer
   attributes :total_revenue
 
   def total_revenue
-    "%.2f" % (object.total_revenue / 100.0)
+    return "%.2f" % (object.total_revenue / 100.0) if object.total_revenue
+    '0'
   end
 end
--- spec/requests/api/v1/merchants/business_intelligence_spec.rb
+++ spec/requests/api/v1/merchants/business_intelligence_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe "Merchants business intelligence API" do
   before(:each) do
+    date1 = "2012-03-16"
+    date2 = "1999-12-31"
     @customer = create(:customer)
     @merchant1 = create(:merchant)
     @merchant2 = create(:merchant)
     @merchant3 = create(:merchant)
-    @invoice1 = create(:invoice, customer: @customer, merchant: @merchant1)
-    @invoice2 = create(:invoice, customer: @customer, merchant: @merchant2)
-    @invoice3 = create(:invoice, customer: @customer, merchant: @merchant3)
+    @invoice1 = create(:invoice, customer: @customer, merchant: @merchant1, created_at: date1)
+    @invoice2 = create(:invoice, customer: @customer, merchant: @merchant2, created_at: date1)
+    @invoice3 = create(:invoice, customer: @customer, merchant: @merchant3, created_at: date2)
     transaction1 = create(:transaction, invoice: @invoice1)
     transaction2 = create(:transaction, invoice: @invoice2)
     transaction3 = create(:transaction, invoice: @invoice3)
@@ -85,28 +87,21 @@ describe "Merchants business intelligence API" do
 
       expect(response).to be_successful
 
-      merchants = JSON.parse(response.body, symbolize_names: true)
+      total_revenue = JSON.parse(response.body, symbolize_names: true)
 
-      expect(merchants.count).to eq(2)
-      expect(merchants.first).to have_key(:id)
-      expect(merchants.first).to have_key(:name)
-      expect(merchants[0][:id]).to eq(@merchant1.id)
-      expect(merchants[1][:id]).to eq(@merchant2.id)
-      expect(merchants.first).to_not have_key(:created_at)
-      expect(merchants.first).to_not have_key(:updated_at)
+      expect(total_revenue).to have_key(:total_revenue)
+      expect(total_revenue[:total_revenue]).to eq("0.08")
+      expect(total_revenue).to_not have_key(:created_at)
+      expect(total_revenue).to_not have_key(:updated_at)
 
-      get "/api/v1/merchants/most_items?quantity=1"
+      get "/api/v1/merchants/revenue?date=2019-03-16"
 
       expect(response).to be_successful
 
-      merchants = JSON.parse(response.body, symbolize_names: true)
+      total_revenue = JSON.parse(response.body, symbolize_names: true)
 
-      expect(merchants.count).to eq(1)
-      expect(merchants.first).to have_key(:id)
-      expect(merchants.first).to have_key(:name)
-      expect(merchants[0][:id]).to eq(@merchant1.id)
-      expect(merchants.first).to_not have_key(:created_at)
-      expect(merchants.first).to_not have_key(:updated_at)
+      expect(total_revenue).to have_key(:total_revenue)
+      expect(total_revenue[:total_revenue]).to eq("0")
     end
   end
 end
