5e6cbf9833f0b63492406b8e7f150c7d705b0f9f
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-08-16 11:07:40 -0600

    creates endpoints for invoices and its relationships

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,14 @@ Rails.application.routes.draw do
         get  '/invoices', to: 'merchant_invoices#index'
       end
 
+      namespace :invoices do
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'
+        get '/random', to: 'random#show'
+      end
+
+      resources :invoices, only: [:index, :show]
+
       resources :items, only: [:index, :show]
       resources :transactions, only: [:index, :show]
     end
