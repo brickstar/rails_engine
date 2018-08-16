e2f3dbdd8b7863c3322933059a8aa840c8ab8ec4
brickstar <brickstar@gmail.com>
2018-08-16 12:51:09 -0600

    item index spec

---
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -0,0 +1,134 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+    item = items.first
+
+    expect(items.count).to eq(3)
+    expect(item).to have_key(:id)
+    expect(item).to have_key(:name)
+    expect(item).to have_key(:description)
+    expect(item).to have_key(:unit_price)
+    expect(item).to have_key(:merchant_id)
+    expect(item).to_not have_key(:created_at)
+    expect(item).to_not have_key(:updated_at)
+  end
+
+  # it "sends a single merchant" do
+  #   merchant = create(:merchant)
+  #
+  #   get "/api/v1/merchants/#{merchant.id}"
+  #
+  #   expect(response).to be_successful
+  #
+  #   merchant = JSON.parse(response.body, symbolize_names: true)
+  #
+  #   expect(merchant).to have_key(:id)
+  #   expect(merchant).to have_key(:name)
+  #   expect(merchant).to_not have_key(:created_at)
+  #   expect(merchant).to_not have_key(:updated_at)
+  # end
+  #
+  # it "sends a single merchant, find by id" do
+  #   merchant = create(:merchant)
+  #
+  #   get "/api/v1/merchants/find?id=#{merchant.id}"
+  #
+  #   expect(response).to be_successful
+  #
+  #   merchant = JSON.parse(response.body, symbolize_names: true)
+  #
+  #   expect(merchant).to have_key(:id)
+  #   expect(merchant).to have_key(:name)
+  # end
+  #
+  # it "sends a single merchant, find by created_at" do
+  #   created_at = "2012-03-27 14:53:59 UTC"
+  #
+  #   merchant = create(:merchant, created_at: created_at)
+  #
+  #   get "/api/v1/merchants/find?created_at=#{merchant.created_at}"
+  #
+  #   expect(response).to be_successful
+  #
+  #   merchant = JSON.parse(response.body, symbolize_names: true)
+  #
+  #   expect(merchant).to have_key(:id)
+  #   expect(merchant).to have_key(:name)
+  #   expect(merchant).to_not have_key(:created_at)
+  #   expect(merchant).to_not have_key(:updated_at)
+  # end
+  #
+  # it "sends a single merchant, find by updated_at" do
+  #   updated_at = "2012-03-27 14:53:59 UTC"
+  #
+  #   merchant = create(:merchant, updated_at: updated_at)
+  #
+  #   get "/api/v1/merchants/find?updated_at=#{merchant.updated_at}"
+  #
+  #   expect(response).to be_successful
+  #
+  #   merchant = JSON.parse(response.body, symbolize_names: true)
+  #
+  #   expect(merchant).to have_key(:id)
+  #   expect(merchant).to have_key(:name)
+  #   expect(merchant).to_not have_key(:created_at)
+  #   expect(merchant).to_not have_key(:updated_at)
+  # end
+  #
+  # it "sends a single merchant, find by name" do
+  #   merchant = create(:merchant)
+  #
+  #   get "/api/v1/merchants/find?name=#{merchant.name}"
+  #
+  #   expect(response).to be_successful
+  #
+  #   merchant = JSON.parse(response.body, symbolize_names: true)
+  #
+  #   expect(merchant).to have_key(:id)
+  #   expect(merchant).to have_key(:name)
+  # end
+  #
+  # it "sends a list of merchants, find_all by name" do
+  #   merchant_name = 'Matt'
+  #
+  #   create(:merchant, name: merchant_name)
+  #   create(:merchant, name: merchant_name)
+  #
+  #   get "/api/v1/merchants/find_all?name=#{merchant_name}"
+  #
+  #   expect(response).to be_successful
+  #
+  #   merchants = JSON.parse(response.body, symbolize_names: true)
+  #   merchant1 = merchants.first
+  #   merchant2 = merchants.last
+  #
+  #   expect(merchants.count).to eq(2)
+  #   expect(merchant1).to have_key(:id)
+  #   expect(merchant1).to have_key(:name)
+  #   expect(merchant1[:name]).to eq('Matt')
+  #   expect(merchant2).to have_key(:id)
+  #   expect(merchant2).to have_key(:name)
+  #   expect(merchant2[:name]).to eq('Matt')
+  # end
+  #
+  # it "sends a random merchant" do
+  #   merchant = create(:merchant)
+  #
+  #   get "/api/v1/merchants/random"
+  #
+  #   expect(response).to be_successful
+  #
+  #   merchant = JSON.parse(response.body, symbolize_names: true)
+  #
+  #   expect(merchant).to have_key(:id)
+  #   expect(merchant).to have_key(:name)
+  # end
+end
