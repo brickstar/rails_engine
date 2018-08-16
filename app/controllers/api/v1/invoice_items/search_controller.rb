4966f685387eb12bf04421365a9edddf8231bbcb
brickstar <brickstar@gmail.com>
2018-08-16 15:38:00 -0600

    add formating for unit price

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -10,6 +10,9 @@ class Api::V1::InvoiceItems::SearchController < ApplicationController
   private
 
   def search_params
+    if params[:unit_price]
+      params[:unit_price] = params[:unit_price].tr('.', '').to_i
+    end
     params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 end
