86cb9360233afb7b913dd813df91f451f800a96b
brickstar <brickstar@gmail.com>
2018-08-15 19:01:04 -0600

    update merchant with most revenue request spec

--- spec/requests/api/v1/merchants/business_intelligence_spec.rb
+++ spec/requests/api/v1/merchants/business_intelligence_spec.rb
@@ -1,30 +1,45 @@
 require 'rails_helper'
 
 describe "Merchants business intelligence API" do
-  it "sends a list of merchants with most revenue" do
-    customer = create(:customer)
-    merchant1 = create(:merchant)
-    merchant2 = create(:merchant)
-    merchant3 = create(:merchant)
-    invoice1 = create(:invoice, customer: customer, merchant: merchant1)
-    invoice2 = create(:invoice, customer: customer, merchant: merchant2)
-    invoice3 = create(:invoice, customer: customer, merchant: merchant3)
-    create_list(:invoice_item, 5, invoice: invoice1)
-    create_list(:invoice_item, 3, invoice: invoice2)
-    create_list(:invoice_item, 1, invoice: invoice3)
+  describe "/merchants/most_revenue" do
+    it "sends a list of merchants with most revenue" do
+      customer = create(:customer)
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+      invoice1 = create(:invoice, customer: customer, merchant: merchant1)
+      invoice2 = create(:invoice, customer: customer, merchant: merchant2)
+      invoice3 = create(:invoice, customer: customer, merchant: merchant3)
+      create_list(:invoice_item, 5, invoice: invoice1)
+      create_list(:invoice_item, 3, invoice: invoice2)
+      create_list(:invoice_item, 1, invoice: invoice3)
 
-    get "/api/v1/merchants/most_revenue?quantity=2"
+      get "/api/v1/merchants/most_revenue?quantity=2"
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(2)
-    expect(merchants.first).to have_key(:id)
-    expect(merchants.first).to have_key(:name)
-    expect(merchants[0][:id]).to eq(merchant1.id)
-    expect(merchants[1][:id]).to eq(merchant2.id)
-    expect(merchants.first).to_not have_key(:created_at)
-    expect(merchants.first).to_not have_key(:updated_at)
+      expect(merchants.count).to eq(2)
+      expect(merchants.first).to have_key(:id)
+      expect(merchants.first).to have_key(:name)
+      expect(merchants[0][:id]).to eq(merchant1.id)
+      expect(merchants[1][:id]).to eq(merchant2.id)
+      expect(merchants.first).to_not have_key(:created_at)
+      expect(merchants.first).to_not have_key(:updated_at)
+
+      get "/api/v1/merchants/most_revenue?quantity=1"
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.count).to eq(1)
+      expect(merchants.first).to have_key(:id)
+      expect(merchants.first).to have_key(:name)
+      expect(merchants[0][:id]).to eq(merchant1.id)
+      expect(merchants.first).to_not have_key(:created_at)
+      expect(merchants.first).to_not have_key(:updated_at)
+    end
   end
 end
