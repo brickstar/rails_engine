b445e25ad29f63ea50975c2bd27a07878b220685
brickstar <brickstar@gmail.com>
2018-08-16 20:39:44 -0600

    total revenue for merchant and total revenue for merchant by date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
 
   def self.with_highest_revenue(limit = 5)
     select("merchants.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue")
@@ -29,5 +31,24 @@ class Merchant < ApplicationRecord
     .where("invoices.created_at BETWEEN ? AND ?", start_date, end_date)
     .order("total_revenue DESC")
     .limit(10).take
+  end
+
+  def total_revenue_for_merchant
+    invoices
+    .joins(:invoice_items, :transactions)
+    .merge(Transaction.unscoped.success)
+    .sum('invoice_items.quantity*invoice_items.unit_price')
+  end
+
+  def total_revenue_for_merchant_by_date(date = "2012-03-16")
+    date = date.to_date
+    start_date = date.beginning_of_day
+    end_date = date.end_of_day
+
+    invoices
+    .joins(:invoice_items, :transactions)
+    .where("invoices.created_at BETWEEN ? AND ?", start_date, end_date)
+    .merge(Transaction.unscoped.success)
+    .sum('invoice_items.quantity*invoice_items.unit_price')
   end
 end
