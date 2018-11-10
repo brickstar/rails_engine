750b104588e77f7a34eaa3320afe613d0180025f
brickstar <brickstar@gmail.com>
2018-11-10 15:19:46 -0700

    Fix seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,23 @@
-source 'https://rubygems.org'
-
-require 'faker'
+10.times do
+  Merchant.create(name: Faker::Company.name)
+end
 
 248.times do
-  create(:item)
+  Item.create(name: Faker::Superhero.name, description: Faker::TwinPeaks.quote, unit_price: Faker::Number.between(1, 20), merchant: Merchant.find(Merchant.pluck(:id).sample))
 end
 
 100.times do
-  create(:customer)
-end
-
-10.times do
-  create(:merchant)
+  Customer.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
 
 484.times do
-  create(:invoice)
+  Invoice.create(customer: Customer.find(Customer.pluck(:id).sample), merchant: Merchant.find(Merchant.pluck(:id).sample), status: 0)
 end
 
 559.times do
-  create(:transaction)
+  Transaction.create(credit_card_number: Faker::Finance.credit_card(:mastercard), result: 1, invoice: Invoice.find(Invoice.pluck(:id).sample))
 end
 
 2168.times do
-  create(:invoice_item)
+ InvoiceItem.create(item: Item.find(Item.pluck(:id).sample), invoice: Invoice.find(Invoice.pluck(:id).sample), quantity: Faker::Number.between(1, 10), unit_price: Faker::Number.between(1, 20))
 end
